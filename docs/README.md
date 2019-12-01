2b41ef24157b6450569cf44c962b7eb0507957d5
AgwaB <leesd556@gmail.com>
2019-12-02 01:49:50 +0900

    add more descrition about parser

--- docs/README.md
+++ docs/README.md
@@ -65,24 +65,24 @@ public class Jsoup {
         con.timeout(timeoutMillis);
         return con.get();
     }
-    public static String clean(String bodyHtml, String baseUri, Whitelist whitelist) {
+    public static String clean(String bodyHtml, String baseUri, Whitelist filter) {
         Document dirty = parseBodyFragment(bodyHtml, baseUri);
-        Cleaner cleaner = new Cleaner(whitelist);
+        Cleaner cleaner = new Cleaner(filter);
         Document clean = cleaner.clean(dirty);
         return clean.body().html();
     }
-    public static String clean(String bodyHtml, Whitelist whitelist) {
-        return clean(bodyHtml, "", whitelist);
+    public static String clean(String bodyHtml, Whitelist filter) {
+        return clean(bodyHtml, "", filter);
     }
-    public static String clean(String bodyHtml, String baseUri, Whitelist whitelist, Document.OutputSettings outputSettings) {
+    public static String clean(String bodyHtml, String baseUri, Whitelist filter, Document.OutputSettings outputSettings) {
         Document dirty = parseBodyFragment(bodyHtml, baseUri);
-        Cleaner cleaner = new Cleaner(whitelist);
+        Cleaner cleaner = new Cleaner(filter);
         Document clean = cleaner.clean(dirty);
         clean.outputSettings(outputSettings);
         return clean.body().html();
     }
-    public static boolean isValid(String bodyHtml, Whitelist whitelist) {
-        return new Cleaner(whitelist).isValidBodyHtml(bodyHtml);
+    public static boolean isValid(String bodyHtml, Whitelist filter) {
+        return new Cleaner(filter).isValidBodyHtml(bodyHtml);
     }
 }
 
@@ -106,6 +106,10 @@ public class Jsoup {
 
 ## Parser
 
+Parser는 `Html` 또는 `Xml`을 String형태로 받아서 알맞은 Tree 형태로 반환하는 역할을 하고 있는 컴포넌트입니다.
+
+
+
 Parser는 크게 다음과 같은 2가지의 패턴으로 이루어져 있습니다. 
 
 - Template Method Pattern
@@ -116,7 +120,7 @@ Parser는 크게 다음과 같은 2가지의 패턴으로 이루어져 있습니
 #### Template Method Pattern
 
 <p align="center">
-    	<img src="./img/parser_template_method.png" width="400" height="200"></img>
+    	<img src="./img/template_method_pattern.png" width="400" height="300"></img>
 </p> 
 
 `TreeBuilder`라는 abstract class를 `HtmlTreeBuilder`와 `XmlTreeBuilder`가 extends하고 있습니다. Html과 Xml의 구조를 만들 때 공통적으로 쓰이는 부분을 추상화 시켜서 추상 클래스를 만들었습니다. 
@@ -125,7 +129,87 @@ Parser는 크게 다음과 같은 2가지의 패턴으로 이루어져 있습니
 
 
 
+#### Command Pattern
+
+`TreeBuilder`를 보면 다음과 같은 abstract method와 해당 process가 Start인지 End인지에 따라서 다른 행위를 취하는 Command Pattern 사용을 유추해 볼 수 있습니다.
+
+```java
+ protected abstract boolean process(Token token);
+
+    protected boolean processStartTag(String name) {
+        if (currentToken == start) { // don't recycle an in-use token
+            return process(new Token.StartTag().name(name));
+        }
+        return process(start.reset().name(name));
+    }
+
+    public boolean processStartTag(String name, Attributes attrs) {
+        if (currentToken == start) { // don't recycle an in-use token
+            return process(new Token.StartTag().nameAttr(name, attrs));
+        }
+        start.reset();
+        start.nameAttr(name, attrs);
+        return process(start);
+    }
+
+    protected boolean processEndTag(String name) {
+        if (currentToken == end) { // don't recycle an in-use token
+            return process(new Token.EndTag().name(name));
+        }
+        return process(end.reset().name(name));
+    }
+
+
+    protected Element currentElement() {
+        int size = stack.size();
+        return size > 0 ? stack.get(size-1) : null;
+    }
+
+```
+
+Command Pattern을 확인하기 위해 `Token` class를 보면 `Token` class는 abstract class임을 알 수 있습니다. 그리고 `Tag`라는 내부 abstract static 클래스를 선언하고 `Token`을 상속 있습니다. 여기서 Tag는 다시 한 번, 내부 static 클래스인 `StartTag`와 `EndTag`에 의해 상속됩니다.
+
+```java
+final static class StartTag extends Tag {
+    ...
+}
+
+final static class EndTag extends Tag{
+    ...
+}
+```
+
+ 그리고 `StartTag`와 `EndTag`는 위에서 언급한 process, processStartTag, processEndTag에서 사용되고 있습니다. 즉, Command Pattern으로 Token 상태에 따라 행위를 결정하고 있습니다.
+
+엄밀히 말하면 추상화된 행위를 구현하는 정석적인 Command Pattern은 아니지만 Token 이라는 추상화된 단위에 따라서 Tag가 Start와 End로 나뉘고 상황에 따라 다른 Tag의 Token을 주입시키고 있습니다.
+
+  <p align="center">
+    	<img src="./img/command_pattern.png" width="400" height="400"></img>
+</p> 
+
+
+
 #### State Pattern
+
+Parser에서는 `HtmlTreeBuilderState`와 `TokeniserState` 에서 State Pattern을 적용하고 있습니다.
+
+Template Method Pattern에서 언급한 `TreeBuilder` 안에는 process(Token token)`라는  abstract method가 존재합니다.
+
+```java
+    protected abstract boolean process(Token token);
+```
+
+그리고 `HtmlTreeBuilder ` 안에는 abstract method의 구현체가 있고 `HtmlTreeBuilderState` 안의 또 다른 abstract method를 실행하고 있습니다.
+
+```java
+@Override
+protected boolean process(Token token) {
+    currentToken = token;
+    return this.state.process(token, this);
+}
+```
+
+
 
 <p align="center">
     	<img src="./img/parser_state_img1.png" width="400" height="700"></img>
@@ -135,32 +219,6 @@ Parser는 크게 다음과 같은 2가지의 패턴으로 이루어져 있습니
 
 ```java
     abstract boolean process(Token t, HtmlTreeBuilder tb);
-
-    private static boolean isWhitespace(Token t) {
-        if (t.isCharacter()) {
-            String data = t.asCharacter().getData();
-            return isWhitespace(data);
-        }
-        return false;
-    }
-
-    private static boolean isWhitespace(String data) {
-        return StringUtil.isBlank(data);
-    }
-
-    private static void handleRcData(Token.StartTag startTag, HtmlTreeBuilder tb) {
-        tb.tokeniser.transition(TokeniserState.Rcdata);
-        tb.markInsertionMode();
-        tb.transition(Text);
-        tb.insert(startTag);
-    }
-
-    private static void handleRawtext(Token.StartTag startTag, HtmlTreeBuilder tb) {
-        tb.tokeniser.transition(TokeniserState.Rawtext);
-        tb.markInsertionMode();
-        tb.transition(Text);
-        tb.insert(startTag);
-    }
 ```
 
 
@@ -197,4 +255,64 @@ Parser는 크게 다음과 같은 2가지의 패턴으로 이루어져 있습니
 
 
 
-즉 `HtmlTreeBuilderState` 는 Html의 tree를 만들 때, tree node의 state에 따라(e.g. Head, Body...) `process` 의 행위가 바뀌도록 하는 `State Pattern`을 사용하고 있습니다.
+그리고 해당 state는 `HtmlTreeBuilder`에서 transition method로 context를 주입받고 있습니다.
+
+```java
+    void transition(HtmlTreeBuilderState state) {
+        this.state = state;
+    }
+```
+
+```java
+void resetInsertionMode() {
+        boolean last = false;
+        for (int pos = stack.size() -1; pos >= 0; pos--) {
+            Element node = stack.get(pos);
+            if (pos == 0) {
+                last = true;
+                node = contextElement;
+            }
+            String name = node.normalName();
+            if ("select".equals(name)) {
+                transition(HtmlTreeBuilderState.InSelect);
+                break; // frag
+            } else if (("td".equals(name) || "th".equals(name) && !last)) {
+                transition(HtmlTreeBuilderState.InCell);
+                break;
+            } else if ("tr".equals(name)) {
+                transition(HtmlTreeBuilderState.InRow);
+                break;
+            } else if ("tbody".equals(name) || "thead".equals(name) || "tfoot".equals(name)) {
+                transition(HtmlTreeBuilderState.InTableBody);
+                break;
+            } else if ("caption".equals(name)) {
+                transition(HtmlTreeBuilderState.InCaption);
+                break;
+            } else if ("colgroup".equals(name)) {
+                transition(HtmlTreeBuilderState.InColumnGroup);
+                break; // frag
+            } else if ("table".equals(name)) {
+                transition(HtmlTreeBuilderState.InTable);
+                break;
+            } else if ("head".equals(name)) {
+                transition(HtmlTreeBuilderState.InBody);
+                break; // frag
+            } else if ("body".equals(name)) {
+                transition(HtmlTreeBuilderState.InBody);
+                break;
+            } else if ("frameset".equals(name)) {
+                transition(HtmlTreeBuilderState.InFrameset);
+                break; // frag
+            } else if ("html".equals(name)) {
+                transition(HtmlTreeBuilderState.BeforeHead);
+                break; // frag
+            } else if (last) {
+                transition(HtmlTreeBuilderState.InBody);
+                break; // frag
+            }
+        }
+    }
+```
+
+ `HtmlTreeBuilderState` 는 Html의 tree를 만들 때, tree node의 state에 따라(e.g. Head, Body...) `HtmlTreeBuilder`의 상태(state)가 바뀌고 있습니다. 즉 `process` 의 행위가 바뀌도록 하는 `State Pattern`을 사용하고 있습니다.
+
--- src/test/java/org/jsoup/safety/CleanerBlacklistTest.java
+++ src/test/java/org/jsoup/safety/CleanerBlacklistTest.java
@@ -20,21 +20,21 @@ public class CleanerTest {
 
     @Test public void simpleBehaviourTest() {
         String h = "<div><p class=foo><a href='http://evil.com'>Hello <b id=bar>there</b>!</a></div>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.simpleText());
+        String cleanHtml = Jsoup.clean(h, Filter.simpleText());
 
         assertEquals("Hello <b>there</b>!", TextUtil.stripNewlines(cleanHtml));
     }
     
     @Test public void simpleBehaviourTest2() {
         String h = "Hello <b>there</b>!";
-        String cleanHtml = Jsoup.clean(h, Whitelist.simpleText());
+        String cleanHtml = Jsoup.clean(h, Filter.simpleText());
 
         assertEquals("Hello <b>there</b>!", TextUtil.stripNewlines(cleanHtml));
     }
 
     @Test public void basicBehaviourTest() {
         String h = "<div><p><a href='javascript:sendAllMoney()'>Dodgy</a> <A HREF='HTTP://nice.com'>Nice</a></p><blockquote>Hello</blockquote>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basic());
+        String cleanHtml = Jsoup.clean(h, Filter.basic());
 
         assertEquals("<p><a rel=\"nofollow\">Dodgy</a> <a href=\"http://nice.com\" rel=\"nofollow\">Nice</a></p><blockquote>Hello</blockquote>",
                 TextUtil.stripNewlines(cleanHtml));
@@ -42,33 +42,33 @@ public class CleanerTest {
     
     @Test public void basicWithImagesTest() {
         String h = "<div><p><img src='http://example.com/' alt=Image></p><p><img src='ftp://ftp.example.com'></p></div>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basicWithImages());
+        String cleanHtml = Jsoup.clean(h, Filter.basicWithImages());
         assertEquals("<p><img src=\"http://example.com/\" alt=\"Image\"></p><p><img></p>", TextUtil.stripNewlines(cleanHtml));
     }
     
     @Test public void testRelaxed() {
         String h = "<h1>Head</h1><table><tr><td>One<td>Two</td></tr></table>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("<h1>Head</h1><table><tbody><tr><td>One</td><td>Two</td></tr></tbody></table>", TextUtil.stripNewlines(cleanHtml));
     }
 
     @Test public void testRemoveTags() {
         String h = "<div><p><A HREF='HTTP://nice.com'>Nice</a></p><blockquote>Hello</blockquote>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basic().removeTags("a"));
+        String cleanHtml = Jsoup.clean(h, Filter.basic().removeTags("a"));
 
         assertEquals("<p>Nice</p><blockquote>Hello</blockquote>", TextUtil.stripNewlines(cleanHtml));
     }
 
     @Test public void testRemoveAttributes() {
         String h = "<div><p>Nice</p><blockquote cite='http://example.com/quotations'>Hello</blockquote>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basic().removeAttributes("blockquote", "cite"));
+        String cleanHtml = Jsoup.clean(h, Filter.basic().removeAttributes("blockquote", "cite"));
 
         assertEquals("<p>Nice</p><blockquote>Hello</blockquote>", TextUtil.stripNewlines(cleanHtml));
     }
 
     @Test public void testRemoveEnforcedAttributes() {
         String h = "<div><p><A HREF='HTTP://nice.com'>Nice</a></p><blockquote>Hello</blockquote>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basic().removeEnforcedAttribute("a", "rel"));
+        String cleanHtml = Jsoup.clean(h, Filter.basic().removeEnforcedAttribute("a", "rel"));
 
         assertEquals("<p><a href=\"http://nice.com\">Nice</a></p><blockquote>Hello</blockquote>",
                 TextUtil.stripNewlines(cleanHtml));
@@ -76,50 +76,50 @@ public class CleanerTest {
 
     @Test public void testRemoveProtocols() {
         String h = "<p>Contact me <a href='mailto:info@example.com'>here</a></p>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basic().removeProtocols("a", "href", "ftp", "mailto"));
+        String cleanHtml = Jsoup.clean(h, Filter.basic().removeProtocols("a", "href", "ftp", "mailto"));
 
         assertEquals("<p>Contact me <a rel=\"nofollow\">here</a></p>",
                 TextUtil.stripNewlines(cleanHtml));
     }
 
     @Test @MultiLocaleTest public void whitelistedProtocolShouldBeRetained() {
-        Whitelist whitelist = Whitelist.none()
+        Filter filter = Filter.none()
                 .addTags("a")
                 .addAttributes("a", "href")
                 .addProtocols("a", "href", "something");
 
-        String cleanHtml = Jsoup.clean("<a href=\"SOMETHING://x\"></a>", whitelist);
+        String cleanHtml = Jsoup.clean("<a href=\"SOMETHING://x\"></a>", filter);
 
         assertEquals("<a href=\"SOMETHING://x\"></a>", TextUtil.stripNewlines(cleanHtml));
     }
 
     @Test public void testDropComments() {
         String h = "<p>Hello<!-- no --></p>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("<p>Hello</p>", cleanHtml);
     }
     
     @Test public void testDropXmlProc() {
         String h = "<?import namespace=\"xss\"><p>Hello</p>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("<p>Hello</p>", cleanHtml);
     }
     
     @Test public void testDropScript() {
         String h = "<SCRIPT SRC=//ha.ckers.org/.j><SCRIPT>alert(/XSS/.source)</SCRIPT>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("", cleanHtml);
     }
     
     @Test public void testDropImageScript() {
         String h = "<IMG SRC=\"javascript:alert('XSS')\">";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("<img>", cleanHtml);
     }
     
     @Test public void testCleanJavascriptHref() {
         String h = "<A HREF=\"javascript:document.location='http://www.google.com/'\">XSS</A>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("<a>XSS</a>", cleanHtml);
     }
 
@@ -127,15 +127,15 @@ public class CleanerTest {
         String validAnchor = "<a href=\"#valid\">Valid anchor</a>";
         String invalidAnchor = "<a href=\"#anchor with spaces\">Invalid anchor</a>";
 
-        // A Whitelist that does not allow anchors will strip them out.
-        String cleanHtml = Jsoup.clean(validAnchor, Whitelist.relaxed());
+        // A Filter that does not allow anchors will strip them out.
+        String cleanHtml = Jsoup.clean(validAnchor, Filter.relaxed());
         assertEquals("<a>Valid anchor</a>", cleanHtml);
 
-        cleanHtml = Jsoup.clean(invalidAnchor, Whitelist.relaxed());
+        cleanHtml = Jsoup.clean(invalidAnchor, Filter.relaxed());
         assertEquals("<a>Invalid anchor</a>", cleanHtml);
 
-        // A Whitelist that allows them will keep them.
-        Whitelist relaxedWithAnchor = Whitelist.relaxed().addProtocols("a", "href", "#");
+        // A Filter that allows them will keep them.
+        Filter relaxedWithAnchor = Filter.relaxed().addProtocols("a", "href", "#");
 
         cleanHtml = Jsoup.clean(validAnchor, relaxedWithAnchor);
         assertEquals(validAnchor, cleanHtml);
@@ -147,13 +147,13 @@ public class CleanerTest {
 
     @Test public void testDropsUnknownTags() {
         String h = "<p><custom foo=true>Test</custom></p>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.relaxed());
+        String cleanHtml = Jsoup.clean(h, Filter.relaxed());
         assertEquals("<p>Test</p>", cleanHtml);
     }
     
     @Test public void testHandlesEmptyAttributes() {
         String h = "<img alt=\"\" src= unknown=''>";
-        String cleanHtml = Jsoup.clean(h, Whitelist.basicWithImages());
+        String cleanHtml = Jsoup.clean(h, Filter.basicWithImages());
         assertEquals("<img alt=\"\">", cleanHtml);
     }
 
@@ -167,74 +167,74 @@ public class CleanerTest {
         String nok5 = "<p>Test <b><a href='http://example.com/' rel='nofollowme'>OK</a></b></p>";
         String nok6 = "<p>Test <b><a href='http://example.com/'>OK</b></p>"; // missing close tag
         String nok7 = "</div>What";
-        assertTrue(Jsoup.isValid(ok, Whitelist.basic()));
-        assertTrue(Jsoup.isValid(ok1, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(nok1, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(nok2, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(nok3, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(nok4, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(nok5, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(nok6, Whitelist.basic()));
-        assertFalse(Jsoup.isValid(ok, Whitelist.none()));
-        assertFalse(Jsoup.isValid(nok7, Whitelist.basic()));
+        assertTrue(Jsoup.isValid(ok, Filter.basic()));
+        assertTrue(Jsoup.isValid(ok1, Filter.basic()));
+        assertFalse(Jsoup.isValid(nok1, Filter.basic()));
+        assertFalse(Jsoup.isValid(nok2, Filter.basic()));
+        assertFalse(Jsoup.isValid(nok3, Filter.basic()));
+        assertFalse(Jsoup.isValid(nok4, Filter.basic()));
+        assertFalse(Jsoup.isValid(nok5, Filter.basic()));
+        assertFalse(Jsoup.isValid(nok6, Filter.basic()));
+        assertFalse(Jsoup.isValid(ok, Filter.none()));
+        assertFalse(Jsoup.isValid(nok7, Filter.basic()));
     }
 
     @Test public void testIsValidDocument() {
         String ok = "<html><head></head><body><p>Hello</p></body><html>";
         String nok = "<html><head><script>woops</script><title>Hello</title></head><body><p>Hello</p></body><html>";
 
-        Whitelist relaxed = Whitelist.relaxed();
+        Filter relaxed = Filter.relaxed();
         Cleaner cleaner = new Cleaner(relaxed);
         Document okDoc = Jsoup.parse(ok);
         assertTrue(cleaner.isValid(okDoc));
         assertFalse(cleaner.isValid(Jsoup.parse(nok)));
-        assertFalse(new Cleaner(Whitelist.none()).isValid(okDoc));
+        assertFalse(new Cleaner(Filter.none()).isValid(okDoc));
     }
     
     @Test public void resolvesRelativeLinks() {
         String html = "<a href='/foo'>Link</a><img src='/bar'>";
-        String clean = Jsoup.clean(html, "http://example.com/", Whitelist.basicWithImages());
+        String clean = Jsoup.clean(html, "http://example.com/", Filter.basicWithImages());
         assertEquals("<a href=\"http://example.com/foo\" rel=\"nofollow\">Link</a>\n<img src=\"http://example.com/bar\">", clean);
     }
 
     @Test public void preservesRelativeLinksIfConfigured() {
         String html = "<a href='/foo'>Link</a><img src='/bar'> <img src='javascript:alert()'>";
-        String clean = Jsoup.clean(html, "http://example.com/", Whitelist.basicWithImages().preserveRelativeLinks(true));
+        String clean = Jsoup.clean(html, "http://example.com/", Filter.basicWithImages().preserveRelativeLinks(true));
         assertEquals("<a href=\"/foo\" rel=\"nofollow\">Link</a>\n<img src=\"/bar\"> \n<img>", clean);
     }
     
     @Test public void dropsUnresolvableRelativeLinks() {
         String html = "<a href='/foo'>Link</a>";
-        String clean = Jsoup.clean(html, Whitelist.basic());
+        String clean = Jsoup.clean(html, Filter.basic());
         assertEquals("<a rel=\"nofollow\">Link</a>", clean);
     }
 
     @Test public void handlesCustomProtocols() {
         String html = "<img src='cid:12345' /> <img src='data:gzzt' />";
-        String dropped = Jsoup.clean(html, Whitelist.basicWithImages());
+        String dropped = Jsoup.clean(html, Filter.basicWithImages());
         assertEquals("<img> \n<img>", dropped);
 
-        String preserved = Jsoup.clean(html, Whitelist.basicWithImages().addProtocols("img", "src", "cid", "data"));
+        String preserved = Jsoup.clean(html, Filter.basicWithImages().addProtocols("img", "src", "cid", "data"));
         assertEquals("<img src=\"cid:12345\"> \n<img src=\"data:gzzt\">", preserved);
     }
 
     @Test public void handlesAllPseudoTag() {
         String html = "<p class='foo' src='bar'><a class='qux'>link</a></p>";
-        Whitelist whitelist = new Whitelist()
+        Filter filter = new Filter()
                 .addAttributes(":all", "class")
                 .addAttributes("p", "style")
                 .addTags("p", "a");
 
-        String clean = Jsoup.clean(html, whitelist);
+        String clean = Jsoup.clean(html, filter);
         assertEquals("<p class=\"foo\"><a class=\"qux\">link</a></p>", clean);
     }
 
     @Test public void addsTagOnAttributesIfNotSet() {
         String html = "<p class='foo' src='bar'>One</p>";
-        Whitelist whitelist = new Whitelist()
+        Filter filter = new Filter()
             .addAttributes("p", "class");
-        // ^^ whitelist does not have explicit tag add for p, inferred from add attributes.
-        String clean = Jsoup.clean(html, whitelist);
+        // ^^ filter does not have explicit tag add for p, inferred from add attributes.
+        String clean = Jsoup.clean(html, filter);
         assertEquals("<p class=\"foo\">One</p>", clean);
     }
 
@@ -246,8 +246,8 @@ public class CleanerTest {
         os.charset("ascii");
 
         String html = "<div><p>&bernou;</p></div>";
-        String customOut = Jsoup.clean(html, "http://foo.com/", Whitelist.relaxed(), os);
-        String defaultOut = Jsoup.clean(html, "http://foo.com/", Whitelist.relaxed());
+        String customOut = Jsoup.clean(html, "http://foo.com/", Filter.relaxed(), os);
+        String defaultOut = Jsoup.clean(html, "http://foo.com/", Filter.relaxed());
         assertNotSame(defaultOut, customOut);
 
         assertEquals("<div><p>&Bscr;</p></div>", customOut); // entities now prefers shorted names if aliased
@@ -257,36 +257,36 @@ public class CleanerTest {
 
         os.charset("ASCII");
         os.escapeMode(Entities.EscapeMode.base);
-        String customOut2 = Jsoup.clean(html, "http://foo.com/", Whitelist.relaxed(), os);
+        String customOut2 = Jsoup.clean(html, "http://foo.com/", Filter.relaxed(), os);
         assertEquals("<div><p>&#x212c;</p></div>", customOut2);
     }
 
     @Test public void handlesFramesets() {
         String dirty = "<html><head><script></script><noscript></noscript></head><frameset><frame src=\"foo\" /><frame src=\"foo\" /></frameset></html>";
-        String clean = Jsoup.clean(dirty, Whitelist.basic());
+        String clean = Jsoup.clean(dirty, Filter.basic());
         assertEquals("", clean); // nothing good can come out of that
 
         Document dirtyDoc = Jsoup.parse(dirty);
-        Document cleanDoc = new Cleaner(Whitelist.basic()).clean(dirtyDoc);
+        Document cleanDoc = new Cleaner(Filter.basic()).clean(dirtyDoc);
         assertNotNull(cleanDoc);
         assertEquals(0, cleanDoc.body().childNodeSize());
     }
 
     @Test public void cleansInternationalText() {
-        assertEquals("привет", Jsoup.clean("привет", Whitelist.none()));
+        assertEquals("привет", Jsoup.clean("привет", Filter.none()));
     }
 
     @Test
     public void testScriptTagInWhiteList() {
-        Whitelist whitelist = Whitelist.relaxed();
-        whitelist.addTags( "script" );
-        assertTrue( Jsoup.isValid("Hello<script>alert('Doh')</script>World !", whitelist ) );
+        Filter filter = Filter.relaxed();
+        filter.addTags( "script" );
+        assertTrue( Jsoup.isValid("Hello<script>alert('Doh')</script>World !", filter) );
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void bailsIfRemovingProtocolThatsNotSet() {
         // a case that came up on the email list
-        Whitelist w = Whitelist.none();
+        Filter w = Filter.none();
 
         // note no add tag, and removing protocol without adding first
         w.addAttributes("a", "href");
@@ -295,20 +295,20 @@ public class CleanerTest {
 
     @Test public void handlesControlCharactersAfterTagName() {
         String html = "<a/\06>";
-        String clean = Jsoup.clean(html, Whitelist.basic());
+        String clean = Jsoup.clean(html, Filter.basic());
         assertEquals("<a rel=\"nofollow\"></a>", clean);
     }
 
     @Test public void handlesAttributesWithNoValue() {
         // https://github.com/jhy/jsoup/issues/973
-        String clean = Jsoup.clean("<a href>Clean</a>", Whitelist.basic());
+        String clean = Jsoup.clean("<a href>Clean</a>", Filter.basic());
 
         assertEquals("<a rel=\"nofollow\">Clean</a>", clean);
     }
 
     @Test public void handlesNoHrefAttribute() {
         String dirty = "<a>One</a> <a href>Two</a>";
-        Whitelist relaxedWithAnchor = Whitelist.relaxed().addProtocols("a", "href", "#");
+        Filter relaxedWithAnchor = Filter.relaxed().addProtocols("a", "href", "#");
         String clean = Jsoup.clean(dirty, relaxedWithAnchor);
         assertEquals("<a>One</a> \n<a>Two</a>", clean);
     }
